1054aa763f6d78aa3bc237d1c5ad19710483b8f4
khj20231204 <natista00@naver.com>
2024-10-18 06:59:32 +0900

    server db dto

---
+++ react-context-jwt-security/server/SQL/user_auth.sql
@@ -0,0 +1,17 @@
+
+-- USER_AUTH : 권한 테이블
+
+CREATE TABLE USER_AUTH(
+	AUTH_NO INT NOT NULL auto_increment PRIMARY KEY,   -- 권한 번호
+   USER_ID VARCHAR(100) NOT NULL,                     -- 아이디
+   AUTH VARCHAR(100) NOT NULL                         -- 권한(USER, ADMIN)
+);
+
+-- 사용자 권한 : USER
+INSERT INTO USER_AUTH(USER_ID, AUTH) VALUES('USER', 'ROLE_USER');
+
+-- 관리자 권한 : USER, ADMIN
+INSERT INTO USER_AUTH(USER_ID, AUTH) VALUES('ADMIN', 'ROLE_ADMIN');
+INSERT INTO USER_AUTH(USER_ID, AUTH) VALUES('ADMIN', 'ROLE_USER');
+
+SELECT * FROM user_auth;
---
+++ react-context-jwt-security/server/src/main/resources/com/joeun/server/dto/Users.java
@@ -0,0 +1,40 @@
+package com.joeun.server.dto;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Users {
+   
+   private int no;
+   private String userId;
+   private String userPw;
+   private String userPwCheck; //비밀번호 체크
+   private String name;
+   private String email;
+   private Date regDate;
+   private Date updDate;
+   private int enabled;    //휴면 여부
+
+   //권한 목록
+   List<UserAuth> authList;
+
+
+   public Users() {
+   }
+
+   public Users(Users user){
+      this.no = user.getNo();
+      this.userId = user.getUserId();
+      this.userPw = user.getUserPw();
+      this.name = user.getName();
+      this.email = user.getEmail();
+      this.regDate = user.getRegDate();
+      this.updDate = user.getUpdDate();
+      this.enabled = user.getEnabled();
+      this.authList = user.getAuthList();
+   }
+   
+}
